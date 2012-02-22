510517cd1fef4b2a0ae5c0132e013e3e72561bc3
chan <atbl1511@hotmail.com>
2012-02-22 00:47:09 -0800

    Add CheckInActivity

---
+++ Spotr/src/com/csun/spotr/CheckInActivity.java
@@ -0,0 +1,12 @@
+package com.csun.spotr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckInActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.check_in);
+	}
+}
